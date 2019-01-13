f8c616d53b9a6a395112993d6cbfbc0b686d8463
amtkxa <scarlet0211@outlook.jp>
2019-01-14 00:23:57 +0900

    コメントを追加

--- src/main/kotlin/com/amtkxa/kotlinsparkrestapi/user/UserService.kt
+++ src/main/kotlin/com/amtkxa/kotlinsparkrestapi/user/UserService.kt
@@ -4,9 +4,36 @@ package com.amtkxa.kotlinsparkrestapi.user
 class UserService {
     private val userRepository = UserRepository()
 
+    /**
+     * Retrieve all user.
+     *
+     * @return list of User
+     */
     fun findAll(): List<User> = userRepository.findAll()
-    fun findById(id: Long): List<User> = userRepository.findById(id)
-    fun create(id: Long, name: String) = userRepository.create(id, name)
-    fun update(id: Long, name: String) = userRepository.update(id, name)
-    fun delete(id: Long) = userRepository.delete(id)
+
+    /**
+     * Retrieve a user with a userId.
+     *
+     * @return list of User
+     */
+    fun findById(id: Long): List<User> = userRepository.findById(id = id)
+
+    /**
+     * Create a user.
+     *
+     * @return userId
+     */
+    fun create(id: Long, name: String) = userRepository.create(id = id, name = name)
+
+    /**
+     * Update a user.
+     *
+     * @return userId
+     */
+    fun update(id: Long, name: String) = userRepository.update(id = id, name = name)
+
+    /**
+     * Delete a user.
+     */
+    fun delete(id: Long) = userRepository.delete(id = id)
 }
