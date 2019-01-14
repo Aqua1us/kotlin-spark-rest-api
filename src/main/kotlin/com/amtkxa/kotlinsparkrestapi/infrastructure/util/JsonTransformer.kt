55684ca7e3c8c7bc33608001433681e3be18764b
amtkxa <scarlet0211@outlook.jp>
2019-01-14 12:44:48 +0900

    objectMapper.writeValueAsString に関するコメントを追加

--- src/main/kotlin/com/amtkxa/kotlinsparkrestapi/infrastructure/util/JsonTransformer.kt
+++ src/main/kotlin/com/amtkxa/kotlinsparkrestapi/infrastructure/util/JsonTransformer.kt
@@ -4,5 +4,10 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import spark.ResponseTransformer
 
 class JsonTransformer(private val objectMapper: ObjectMapper) : ResponseTransformer {
+    /**
+     * Generates a JSON from a Java object and returns the generated JSON as a string.
+     *
+     * @return list of User
+     */
     override fun render(model: Any?): String = objectMapper.writeValueAsString(model)
 }
